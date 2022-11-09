0df84c8217a78fad673609f8475d89150d6312c3
Даниил <dragon0lordchik@gmail.com>
2022-11-09 18:52:24 +0300

    temp

---
+++ SchoolWhiteWings/Pages/TeacherPages/TeacherStartPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using SchoolWhiteWings.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolWhiteWings
+{
+    /// <summary>
+    /// Interaction logic for TeacherStartPage.xaml
+    /// </summary>
+    public partial class TeacherStartPage : Page
+    {
+        public Teacher teacher { get; set; }
+        public TeacherStartPage(Teacher oldTeacher)
+        {
+            InitializeComponent();
+            teacher = oldTeacher;
+
+            UserNameBlock.Text = teacher.FirstName + " " + teacher.LastName;
+        }
+    }
+}
