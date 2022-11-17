d5eed1f227e5a0a0f24020b93cb047b4133a3dc8
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-11-17 12:38:05 +0300

    Add go out of app and design list

--- SchoolWhiteWings/Pages/TeacherPages/TeacherStartPage.xaml.cs
+++ SchoolWhiteWings/Pages/TeacherPages/TeacherStartPage.xaml.cs
@@ -22,13 +22,35 @@ namespace SchoolWhiteWings
     /// </summary>
     public partial class TeacherStartPage : Page
     {
+        public static List<Lesson> lessons { get; set; }
+        private static List<TeacherForSection> _tfs { get; set; }
+        private HashSet<int> sects { get; set; }
         public Teacher teacher { get; set; }
         public TeacherStartPage(Teacher oldTeacher)
         {
             InitializeComponent();
             teacher = oldTeacher;
+            _tfs = MainWindow.db.TeacherForSection.Where(t => t.TeacherId == oldTeacher.Id).ToList();
+            sects = new HashSet<int>();
+            foreach (var temp in _tfs)
+            {
+                sects.Add(temp.SectionId);
+            }
 
+            lessons = MainWindow.db.Lesson.Where(t => sects.Contains((int)t.SectionId)).ToList();
             UserNameBlock.Text = $" Преподаватель: {teacher.FirstName} { teacher.LastName}";
+            this.DataContext = this;
+        }
+        private void lvLessons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var lesson = lvLessons.SelectedItem as Lesson;
+
+            NavigationService.Navigate(new JournalPage(lesson));
+        }
+
+        private void cbLessonIsBe_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.db.SaveChanges();
         }
 
         private void ToSectionTable_Click(object sender, RoutedEventArgs e)
