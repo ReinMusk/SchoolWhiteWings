e7bdcdbdfac0fd42013ad7b96b118d919408ae46
Valdemar-iatneh <avvakumov112003@gmail.com>
2022-11-18 06:44:09 +0300

    Добавил импорт статистики в Excel

--- SchoolWhiteWings/Pages/AdminPages/StatisticksPage.xaml.cs
+++ SchoolWhiteWings/Pages/AdminPages/StatisticksPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace SchoolWhiteWings.Pages.AdminPages
 {
@@ -23,9 +24,10 @@ namespace SchoolWhiteWings.Pages.AdminPages
         public StatisticksPage()
         {
             InitializeComponent();
-            SectionPopLV.ItemsSource = MainWindow.db.Section
+            var sectionPop = MainWindow.db.Section
                 .Where(a => a.isDeleted != true)
                 .OrderByDescending(a => a.Group.Count).ToList();
+            SectionPopLV.ItemsSource = sectionPop;
             
             var students = MainWindow.db.Student.ToList();
             var orderedStud = 
@@ -53,6 +55,66 @@ namespace SchoolWhiteWings.Pages.AdminPages
         private void ToPreviousPage_Back(object sender, RoutedEventArgs e)
         {
             NavigationService.GoBack();
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            if (SortsCB.SelectedIndex == 0)
+            {
+                Excel.Application application = new Excel.Application();
+                Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+                Excel.Worksheet worksheet = application.Worksheets.Item[1];
+                int startRowIndex = 1;
+
+                worksheet.Cells[1][startRowIndex] = "Кружок";
+                worksheet.Cells[2][startRowIndex] = "Количество групп";
+                worksheet.Cells[3][startRowIndex] = "Количество учащихся";
+                startRowIndex++;
+
+                var sectionPop = MainWindow.db.Section
+                    .Where(a => a.isDeleted != true)
+                    .OrderByDescending(a => a.Group.Count).ToList();
+                foreach (var result in sectionPop)
+                {
+                    worksheet.Cells[1][startRowIndex] = result.Name;
+                    worksheet.Cells[2][startRowIndex] = result.Group.Count;
+                    worksheet.Cells[3][startRowIndex] = result.StudentCount;
+                    startRowIndex++;
+                }
+                worksheet.Columns.AutoFit();
+                worksheet.Rows.AutoFit();
+                startRowIndex = 1;
+                application.Visible = true;
+            }
+            else if (SortsCB.SelectedIndex == 1)
+            {
+                Excel.Application application = new Excel.Application();
+                Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+                Excel.Worksheet worksheet = application.Worksheets.Item[1];
+                int startRowIndex = 1;
+                worksheet.Rows.HorizontalAlignment = HorizontalAlignment.Center;
+                worksheet.Rows.VerticalAlignment = HorizontalAlignment.Center;
+                worksheet.Cells[1][startRowIndex] = "ФИО";
+                worksheet.Cells[2][startRowIndex] = "Класс";
+                worksheet.Cells[3][startRowIndex] = "Общее количество посещений";
+                startRowIndex++;
+
+                var students = MainWindow.db.Student.ToList();
+                var orderedStud =
+                    from s in students
+                    orderby s.JournalCount descending
+                    select s;
+                foreach (var result in orderedStud)
+                {
+                    worksheet.Cells[1][startRowIndex] = result.FirstName + " " + result.LastName + " " + result.Patronymic;
+                    worksheet.Cells[2][startRowIndex] = result.Class.Number;
+                    worksheet.Cells[3][startRowIndex] = result.JournalCount;
+                }
+                worksheet.Columns.AutoFit();
+                worksheet.Rows.AutoFit();
+                startRowIndex = 1;
+                application.Visible = true;
+            }
         }
     }
 }
